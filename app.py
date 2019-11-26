362cd3559a3643164a65905e981f422c2eb1ae07
RajvardhanPalekar <rajvardhan.palekar@gmail.com>
2019-11-26 13:29:04 +0000

    Adding some static data and html templates with Bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+groups = [
+    {
+        'name': 'Marvel Family',
+        'admin': 'Tony Stark',
+        'participants': 8
+    },
+    {
+        'name': 'Health Buddies',
+        'admin': 'Bruce Banner',
+        'participants': 3
+    },
+    {
+        'name': 'Work Friends',
+        'admin': 'Steve Rogers',
+        'participants': 10
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', groups=groups)
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
