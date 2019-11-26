10deb5e2d222486d7a86e8bd2d0cb567db267933
RajvardhanPalekar <rajvardhan.palekar@gmail.com>
2019-11-26 16:32:06 +0000

    Adding login and registration forms

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "d2f5cc0afca8ffb79e5cdc7a39181329"
 
 groups = [
     {
         'name': 'Avengers Family',
-        'admin': 'Tony Stark',
+        'admin': 'starkTony3000',
         'participants': 6
     },
     {
         'name': 'Workout Buddies',
-        'admin': 'Bruce Banner',
+        'admin': 'angryBanner1',
         'participants': 5
     },
     {
         'name': 'Work Friends',
-        'admin': 'Steve Rogers',
+        'admin': 'captain$Rogers',
         'participants': 10
     }
 ]
@@ -27,6 +30,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Hello {form.username.data}, Welcome to FlaskApp!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@flaskapp.com' and  form.password.data == 'password':
+            flash(f'Welcome back admin!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Invalid email or password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
